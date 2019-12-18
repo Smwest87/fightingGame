bf83aaa8748f0ffe5673a407ccb643d06d1d9491
Smwest87 <steven@shipt.com>
2019-12-17 19:09:06 -0600

    Added basic jab function

--- fightingGame.go
+++ fightingGame.go
@@ -5,25 +5,40 @@ import (
 	"fmt"
 )
 
-type character struct {
-	position int
-	speed    int
-	hp       int
+type position struct {
+	x int
+	y int
 }
+
+type character struct {
+	position
+	speed  int
+	hp     int
+	attack int
+}
+
 type level struct {
 	size int
 }
 
 func (p *character) movePlayer(playerOne *character, playerTwo *character, l *level, distance int) error {
 
-	if playerOne.position+distance <= 0 || playerOne.position+distance >= l.size {
-		return fmt.Errorf("Player cannot leave the map, Player New Position : %v", playerOne.position+distance)
+	if playerOne.position.x+distance <= 0 || playerOne.position.x+distance >= l.size {
+		return fmt.Errorf("Player cannot leave the map, Player New Position : %v", playerOne.position.x+distance)
 	}
 
-	if playerOne.position+distance == playerTwo.position {
+	if playerOne.position.x+distance == playerTwo.position.x {
 		return errors.New("players cannot have the same position")
 	}
-	playerOne.position += distance
+	playerOne.position.x += distance
 
 	return nil
+}
+
+func (p *character) jab(opponent *character, l *level) error {
+	if p.position.x-opponent.position.x == 1 || p.position.x-opponent.position.x == -1 {
+		opponent.hp -= p.attack
+		return nil
+	}
+	return errors.New("Opponent out of range")
 }
