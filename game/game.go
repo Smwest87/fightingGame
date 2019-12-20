12f81159eb21a1232df392d39836d069c43e4cdd
Smwest87 <steven@shipt.com>
2019-12-20 13:54:17 -0600

    Some basic rendering

--- game/game.go
+++ game/game.go
@@ -2,11 +2,17 @@ package game
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/smwest87/fightingGame/fighter"
 	"github.com/smwest87/fightingGame/level"
 )
+
+var headLine string = "              0               0      "
+var bodyLine string = "             /|\\             /|\\    "
+var weapon string = "             (o-\"=         =\"-o)    "
+var legs string = "-------------/ \\-------------/ \\--------"
 
 type Game struct {
 	time      int
@@ -14,6 +20,7 @@ type Game struct {
 	playerOne fighter.Character
 	playerTwo fighter.Character
 	State     GameState
+	Frame     string
 }
 
 type GameState struct {
@@ -33,21 +40,44 @@ func (g Game) GameStart(capacity int, start chan GameState) {
 
 func (g Game) RunGame() {
 
-	start := make(chan GameState, 60)
+	g.playerOne.Hp = 1
+	g.playerOne.Position.X = 1
 
+	g.playerTwo.Hp = 1
+	g.playerTwo.Position.X = 10
 	fmt.Println("---------------------Ready?---------------------")
 	fmt.Println("---------------------Fight!---------------------")
 
-	g.playerOne.Hp = 1
-	g.playerTwo.Hp = 1
-	go g.GameStart(cap(start), start)
+	clock := make(chan GameState, 5)
 
-	for stamp := range start {
+	go g.GameStart(cap(clock), clock)
+
+	for stamp := range clock {
 		if g.playerOne.Hp > 0 || g.playerTwo.Hp > 0 || stamp.GameClock != 0 {
-			fmt.Println(stamp.GameClock)
+			print("\033[H\033[2J")
+			g.State.GameClock = stamp.GameClock
+			println(g.StartFrame())
 		}
 	}
 
-	fmt.Println("---------------------Game Over!---------------------")
+	fmt.Println("\r\n\r\n---------------------Game Over!---------------------")
 
+}
+
+func (g Game) StartFrame() string {
+
+	renderP1 := "|"
+	p1Position := g.playerOne.Position.X
+
+	renderP2 := "|"
+	p2Position := g.playerTwo.Position.X
+
+	ground := []string{"-", "-", "-", "-", "-", "-", "-", "-", "-", "-"}
+
+	ground[p1Position-1] = renderP1
+	ground[p2Position-1] = renderP2
+
+	return fmt.Sprintf("     %v     \r\n%v", g.State.GameClock, strings.Join(ground, ""))
+
+	//return fmt.Sprintf("                     %v       \r\n\r\n\r\n\r\n\r\n%v\r\n%v\r\n%v\r\n%v", g.State.GameClock, headLine, bodyLine, weapon, legs)
 }
