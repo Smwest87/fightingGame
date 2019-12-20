4303f26dcf46b39252d096e729a650c351017ed0
Smwest87 <steven@shipt.com>
2019-12-20 10:49:54 -0600

    New function RunGame and added checks on player HP

--- game/game.go
+++ game/game.go
@@ -1,6 +1,7 @@
 package game
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/smwest87/fightingGame/fighter"
@@ -16,16 +17,37 @@ type Game struct {
 }
 
 type GameState struct {
-	timeStamp time.Time
-	gameClock int
+	TimeStamp time.Time
+	GameClock int
 }
 
 func (g Game) GameStart(capacity int, start chan GameState) {
-	for g.State.gameClock = capacity; g.State.gameClock >= 0; g.State.gameClock-- {
-		g.State.timeStamp = time.Now()
+	for g.State.GameClock = capacity; g.State.GameClock >= 0; g.State.GameClock-- {
+		g.State.TimeStamp = time.Now()
 		start <- g.State
 		time.Sleep(1000 * time.Millisecond)
 
 	}
 	close(start)
+}
+
+func (g Game) RunGame() {
+
+	start := make(chan GameState, 60)
+
+	fmt.Println("---------------------Ready?---------------------")
+	fmt.Println("---------------------Fight!---------------------")
+
+	g.playerOne.Hp = 1
+	g.playerTwo.Hp = 1
+	go g.GameStart(cap(start), start)
+
+	for stamp := range start {
+		if g.playerOne.Hp > 0 || g.playerTwo.Hp > 0 || stamp.GameClock != 0 {
+			fmt.Println(stamp.GameClock)
+		}
+	}
+
+	fmt.Println("---------------------Game Over!---------------------")
+
 }
